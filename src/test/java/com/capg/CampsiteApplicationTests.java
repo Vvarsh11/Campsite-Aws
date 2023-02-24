97b6b7e0975ad5f8c549d12f5c65eedba4a65b94
Harika <alamanda.harika@capgemini.com>
2023-02-24 23:24:40 +0530

    firstcommit

--- src/test/java/com/capg/CampsiteApplicationTests.java
+++ src/test/java/com/capg/CampsiteApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CampsiteApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+//	@Test
+//	void contextLoads() {
+//	}
+//
 }
